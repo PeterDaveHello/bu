b65b240a6098ebb495e89923f7a2789450395c83
Peter Dave Hello <hsu@peterdavehello.org>
2016-01-03 21:58:32 +0800

    add test/ns_to_sec.bu.sh

---
+++ test/ns_to_sec.bu.sh
@@ -0,0 +1,4 @@
+bu_assert "bu_nano_sec_to_sec 0" "0"
+bu_assert "bu_nano_sec_to_sec 1000000000" "1"
+bu_assert "bu_nano_sec_to_sec 1234000000" "1.234"
+bu_assert "bu_nano_sec_to_sec 1234567890" "1.23456789"
